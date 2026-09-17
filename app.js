cd9ffcbd958639153873e42cef84dee1b5f3cb6f
Noel Alfonso Obando Martinez <147889470+DigitalGenius27@users.noreply.github.com>
2026-09-17 15:57:36 -0600

    restore health check

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...')
   res.send('ok')
-})
+}
+)
 
 const start = async () => {
   await app.listen(PORT)
